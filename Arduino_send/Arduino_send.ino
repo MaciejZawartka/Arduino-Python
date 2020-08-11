0f4c795cddaaad84220732aae7b641bf83c8e356
MaciejZawartka <em.zawartka@gmail.com>
2020-08-11 21:21:55 +0200

    Komunikacja 3 sensory

--- Arduino_send/Arduino_send.ino
+++ Arduino_send/Arduino_send.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+//  Odczytywanie wartosci i wysylanie do PC
+//  Maciej Zawartka
 
+
+// Zmienne
+unsigned long czas_ostatni = 0;
+long napetle = 5000;
+
+//------------------------
+void setup() {
+  Serial.begin(38400);
+  czas_ostatni = micros();   // pomiar czasu
+}
+//------------------------
+void loop() {
+  if (micros() - czas_ostatni >= napetle){
+    double val = (analogRead(0) - 512)/512.0;
+    sendmessage(&val);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void sendmessage(int* data){
+  byte* byteData = (byte*)(data);
+  Serial.write(byteData,2);
+  }
 
+void sendmessage(int* data){
+  byte* byteData = (byte*)(data);
+  Serial.write(byteData,4);
 }
